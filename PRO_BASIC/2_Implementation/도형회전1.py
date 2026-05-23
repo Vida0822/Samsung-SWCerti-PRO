4ca2b87283b0187a94f61d4257d642a050961724
Heemin0822 <sinhimin11@naver.com>
2026-05-23 13:17:04 +0900

    해설 주석 추가

--- PRO_BASIC/2_Implementation/도형회전1.py
+++ PRO_BASIC/2_Implementation/도형회전1.py
@@ -1,5 +1,16 @@
+"""
+도형 회전
+관점1) B배열을 A에서 가져와 채우는 방식 (B배열에 대해 for 문 돌면서 A 배열에서 값 가져오기)
+90º: B[i][j] = A[N-1-j][+i] --> B배열에서 col이 1 증가할 때 A 배열에서 row가 1 감소하므로  -j
+180º : B[i][j] = A[-i][-j]
+ㄴ tip : 증감 방향이 서로 반대면 -, 같으면 +
+
+관점1) A배열의 값을 B로 채우는 방식 (A배열에 대해 for문 돌면서 B 배열에 채워넣기]
+90º: B[j][H-1-i] = A[i][j]
+"""
+
 def rotate_90():
-    global W, H, arr
+    global W, H, arr # 이거 헷갈리니까 담부턴 그냥 N, M (원래 쓰던거)
 
     # copy
     new_arr = [[0 for _ in range(H)] for _ in range(W)]
@@ -59,8 +70,3 @@ else :
 print(W, H)
 for i in range(H):
     print(*arr[i], sep="")
-
-
-
-
-
